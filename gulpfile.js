0e6b95c2e8f5a657f0015aa904095de336e722e3
Jeremy Schulz <Jeremy.Schulz@noaa.gov>
2023-06-30 11:21:08 -0600

    Update to more recent dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@
 
 // Dependencies
 const gulp = require('gulp');
-const runSequence = require('run-sequence');
-const babel = require('rollup-plugin-babel');
+const babel = require('@rollup/plugin-babel');
+
 const rollup = require('rollup');
 const iife = require("gulp-iife");
 const uglify = require('gulp-uglify');
@@ -19,36 +19,23 @@ const licenseText = '/*' + fs.readFileSync('./LICENSE.txt', 'utf8') + '\n*/\n';
 // const packageJson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 
 gulp.task('build', function (cb) {
-    rollup.rollup({
-        entry: 'src/modules/index.js',
-        plugins: [
-            babel({
-                plugins: [
-                    'external-helpers'
-                ],
-                presets: [
-                      [
-                            'es2015',
-                            {
-                                'modules': false
-                            }
-                      ]
-                ]
-            })
-        ]
-    }).then(function (bundle) {
-        bundle.write({
-            dest: 'src/gifshot.js',
-            format: 'es'
-        });
-
-        cb();
-    }).catch(console.error); // log errors;
+  return rollup.rollup({
+		input: 'src/modules/index.js',
+		plugins: [
+			babel({ babelHelpers: 'bundled' })
+		],
+	}).then(function(bundle){
+		bundle.write({
+			file: 'src/gifshot.js',
+			format: 'es'
+		});
+		cb();
+	}).catch(console.error); // log errors;
 });
 
 // Wrap IIFE
 gulp.task('iife', function () {
-    return gulp.src('src/gifshot.js')
+	return gulp.src('src/gifshot.js')
         .pipe(iife({
             params: ['window', 'document', 'navigator', 'undefined'],
             args: ['typeof window !== "undefined" ? window : {}', 'typeof document !== "undefined" ? document : { createElement: function() {} }', 'typeof window !== "undefined" ? window.navigator : {}']
@@ -58,7 +45,7 @@ gulp.task('iife', function () {
 
 // Task that runs the Mocha unit tests and Instanbul test coverage
 gulp.task('test', function (cb) {
-    gulp.src('tests/tests.js')
+    return gulp.src('tests/tests.js')
         .pipe(mocha({
             reporter: 'nyan'
         }))
@@ -84,13 +71,11 @@ gulp.task('minify', function () {
 });
 
 // The default build task (called when you run `gulp`)
-gulp.task('default', function () {
-    runSequence('build', 'iife', 'test', 'copy', 'minify');
-});
+gulp.task('default', gulp.series('build', 'iife', 'test', 'copy', 'minify'));
 
 // The watch task that runs the default task on any gifshot module file changes
 gulp.task('watch', function () {
-    const watcher = gulp.watch('src/modules/**/*.js', ['default']);
+    const watcher = gulp.watch('src/modules/**/*.js', gulp.series('default'));
 
     watcher.on('change', function (event) {
         console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
