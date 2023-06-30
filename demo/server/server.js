1f9d42ad5ccd95b2edf5b65e1873f01213c9eeab
Jeremy Schulz <Jeremy.Schulz@noaa.gov>
2023-06-30 12:14:55 -0600

    Auto open browser on preview

--- demo/server/server.js
+++ demo/server/server.js
@@ -1,10 +1,13 @@
 // server.js (Express 4.0)
-var express = require('express'),
-  app = express();
+const express = require('express');
+const open = require('open');
 
+app = express();
 // SERVER CONFIGURATION
 app.use(express.static(__dirname + '/../')); // set the static files location /public/img will be /img for users
 
 app.listen(8001);
+// opens the url in the default browser
+open('http://localhost:8001');
 
 console.log('To preview gifshot, go to localhost:8001');
